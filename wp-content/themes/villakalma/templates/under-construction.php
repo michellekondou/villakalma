3cba7bfd7fe70f9c5409f9a70a3f186160d56b41
michellekondou <michellekondou@gmail.com>
2016-12-04 23:19:44 +0200

    add wp-sync-db ;

---
+++ wp-content/themes/villakalma/templates/under-construction.php
@@ -0,0 +1,26 @@
+<?php /*
+Template Name: Under Construction Template
+*/
+?><!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700&amp;subset=greek" rel="stylesheet">
+  <link rel='stylesheet' id='sage/css-css'  href='//localhost:3000/wp-content/themes/villakalma/dist/styles/main.css' type='text/css' media='all' />
+  <title>Villakalma under construction</title>
+</head>
+<body>
+    <div class="wrap container" role="document">
+      <div class="content row">
+        <main class="main">
+          <div class="page-header">
+            <h1>Villakalma</h1>
+            <p>
+            under construction 
+            </p>
+          </div>
+        </main>
+      </div>
+    </div>
+</body>
+</html>
