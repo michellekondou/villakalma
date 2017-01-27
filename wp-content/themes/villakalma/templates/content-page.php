364b5bf20b6b30fe7e7b69f9bd56dd83f888baa4
michellekondou <michellekondou@gmail.com>
2017-01-27 12:03:07 +0200

    add photoswipe caption from img title

--- wp-content/themes/villakalma/templates/content-page.php
+++ wp-content/themes/villakalma/templates/content-page.php
@@ -63,7 +63,7 @@ if( $images ):
 <?php foreach( $images as $image ): ?>
   <a data-size="<?php echo $image['width'].'x'.$image['height'] ?>" href="<?php echo $image['url']; ?>">
     <img src="<?php echo $image['sizes']['large']; ?>" alt="<?php echo $image['caption']; ?>"  title="<?php echo $image['caption']; ?>" class="preload">
-    <span class="caption">
+    <span class="caption wp-caption-text">
       <p><?php echo $image['caption']; ?></p>
     </span>
   </a>
@@ -264,7 +264,7 @@ if( $images ):
     <div class="grid-item grid-item--width2 grid-item--width3 grid-item--width4">
       <a data-size="<?php echo $image['width'].'x'.$image['height'] ?>" href="<?php echo $image['url']; ?>">
         <img src="<?php if($image['width'] > $image['height']){ echo $image['sizes']['gallery-lg-ls']; } else { echo $image['sizes']['gallery-lg-pt']; } ?>" alt="<?php echo $image['caption']; ?>" class="preload" title="<?php echo $image['caption']; ?>" />
-        <span class="caption">
+        <span class="caption wp-caption-text">
           <p><?php echo $image['caption']; ?></p>
         </span>
       </a>
