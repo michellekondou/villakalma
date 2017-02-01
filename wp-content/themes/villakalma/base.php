33d3b92e2a98cf193ce95ee1e0aa89a4b22af789
michellekondou <michellekondou@gmail.com>
2017-02-01 14:32:51 +0200

    add analytics tracking code

--- wp-content/themes/villakalma/base.php
+++ wp-content/themes/villakalma/base.php
@@ -9,6 +9,9 @@ use Roots\Sage\Wrapper;
 <html <?php language_attributes(); ?> class="no-js">
   <?php get_template_part('templates/head'); ?>
   <body <?php body_class(); ?>>
+  <?php
+    get_template_part('templates/analyticstracking.php');
+  ?>
     <!--[if IE]>
       <div class="alert alert-warning">
         <?php _e('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'sage'); ?>
